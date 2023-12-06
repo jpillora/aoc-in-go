872e12e40e1d82fc8fed1062e0cc666b76d80481
Jaime Pillora <dev@jpillora.com>
2023-12-06 23:45:08 +1100

    init day 5/6

---
+++ 2023/05/code.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/jpillora/puzzler/harness/aoc"
+)
+
+func main() {
+	aoc.Harness(run)
+}
+
+// on code change, run will be executed 4 times:
+// 1. with: false (part1), and example input
+// 2. with: true (part2), and example input
+// 3. with: false (part1), and user input
+// 4. with: true (part2), and user input
+// the return value of each run is printed to stdout
+func run(part2 bool, input string) any {
+	// when you're ready to do part 2, remove this "not implemented" block
+	if part2 {
+		return "not implemented"
+	}
+	// solve part 1 here
+	return 42
+}
