e3be8da4a9e80e76e8451b11ab13676d09d4ec7c
Jaime Pillora <dev@jpillora.com>
2023-12-06 23:38:59 +1100

    solved 4p2

--- 2023/04/code.go
+++ 2023/04/code.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"strconv"
 	"strings"
 
@@ -13,31 +14,54 @@ func main() {
 
 func run(part2 bool, input string) any {
 	if part2 {
-		return "not implemented"
+		return sumCopies(input)
+	}
+	return sumAllWins(input)
+}
+
+func sumCopies(input string) int {
+	games := strings.Split(input, "\n")
+	// make a mirror list of copy counts
+	copies := make([]int, len(games))
+	for i := range copies {
+		copies[i] = 1
 	}
 	sum := 0
-	for _, line := range strings.Split(input, "\n") {
-		sum += sumGame(line)
+	for i, line := range games {
+		c := copies[i]
+		w := countWins(line)
+		for j := 1; j <= w; j++ {
+			copies[i+j] += c
+		}
+		sum += c
 	}
 	return sum
 }
 
-func sumGame(line string) int {
+func sumAllWins(input string) int {
+	sum := 0
+	for _, line := range strings.Split(input, "\n") {
+		sum += sumWins(line)
+	}
+	return sum
+}
+
+func sumWins(line string) int {
+	return int(math.Pow(2, float64(countWins(line))))
+}
+
+func countWins(line string) int {
 	idSets := strings.SplitN(line, ":", 2)
 	winHas := strings.SplitN(idSets[1], "|", 2)
 	win := set(winHas[0])
 	has := set(winHas[1])
-	sum := 0
+	count := 0
 	for n := range has {
 		if win[n] {
-			if sum == 0 {
-				sum = 1
-			} else {
-				sum *= 2
-			}
+			count++
 		}
 	}
-	return sum
+	return count
 }
 
 func set(s string) map[int]bool {
